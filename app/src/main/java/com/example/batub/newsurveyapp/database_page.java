0c99eb39166178dc2f518aa4a49423408eb6ba65
batubsrn <36736313+batubsrn@users.noreply.github.com>
2018-08-02 15:53:12 +0300

    end day w5d4
    voting done

--- app/src/main/java/com/example/batub/newsurveyapp/database_page.java
+++ app/src/main/java/com/example/batub/newsurveyapp/database_page.java
@@ -1,7 +1,6 @@
 package com.example.batub.newsurveyapp;
 
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,23 +12,31 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class database_page extends AppCompatActivity {
 
     FirebaseDatabase  myDatabase;
-    DatabaseReference voteRef,op1ref,op2ref;
+    DatabaseReference voteRef,op1ref,increment;
     Button vote1button,vote2button;
-    TextView vote1text,vote2text, vote1percentview,vote2percentview,vote1bartext,vote2bartext;
+    TextView vote1text,vote2text, vote1displaytextview,vote2displaytextview,vote1bartext,vote2bartext;
 
     ProgressBar percent1Progress,percent2Progress;
+    // String progbar1textStr,progbar2textStr;
 
     Long vote1Int,vote2Int;
-    Long percent1,percent2;
+    //  Long percent1,percent2;
+
+     static   int i3;
+
+     static Double d3;
+     static Double d4;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.database_page_layout);
@@ -37,8 +44,12 @@ public class database_page extends AppCompatActivity {
         myDatabase=FirebaseDatabase.getInstance();
         voteRef=myDatabase.getReference().child("votecount").child("1");
 
-        op1ref=myDatabase.getReference().child("votecount").child("1").child("vote1");
-        op2ref=myDatabase.getReference().child("votecount").child("1").child("vote2");
+        op1ref=myDatabase.getReference().child("votecount").child("1"); // read vote count
+
+        increment=myDatabase.getReference().child("votecount").child("1"); // increment vote count
+
+        vote1button = (Button) findViewById(R.id.vote1increment);
+        vote2button = (Button) findViewById(R.id.vote2increment);
 
 
         percent1Progress = (ProgressBar) findViewById(R.id.percent1Bar) ;
@@ -49,52 +60,45 @@ public class database_page extends AppCompatActivity {
 
         vote1text= (TextView) findViewById(R.id.vote1text);
         vote2text= (TextView) findViewById(R.id.vote2text);
-        vote1percentview= (TextView) findViewById(R.id.vote1percent);
-        vote2percentview= (TextView) findViewById(R.id.vote2percent);
+
+        vote1displaytextview = (TextView) findViewById(R.id.vote1displaytext);
+        vote2displaytextview= (TextView) findViewById(R.id.vote2displaytext);
 
 
         op1ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                vote1Int = dataSnapshot.getValue(Long.class);
+                vote1Int = dataSnapshot.child("vote1").getValue(Long.class);
+                vote2Int = dataSnapshot.child("vote2").getValue(Long.class);
+
                 String vote1CountStr = String.valueOf(vote1Int);
-                vote1text.setText(vote1CountStr);
-
-                int i = vote1Int.intValue();
-                percent1Progress.setProgress(i);
-                vote1bartext.setText(vote1CountStr);
-
-                /*percent1= (  ( ( vote1Int ) /  (vote1Int+vote2Int)  ) * 100  );
-                String percent1Str =String.valueOf(percent1);
-                vote1percentview.setText( percent1Str );*/
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-        op2ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                vote2Int= dataSnapshot.getValue(Long.class);
                 String vote2CountStr=String.valueOf(vote2Int);
+
+                vote1text.setText(vote1CountStr);
                 vote2text.setText( vote2CountStr );
 
-                int i2 = vote2Int.intValue();
-                percent2Progress.setProgress(i2);
-                vote2bartext.setText(vote2CountStr);
+                d3=vote1Int.doubleValue(); //  vote 1 real count
+                 d4=vote2Int.doubleValue();         // vote 2 real count
 
-                /*percent2= (  ( ( vote2Int ) /  (vote1Int+vote2Int)  ) * 100  );
-                String percent2Str =String.valueOf(percent2);
-                vote2percentview.setText( percent2Str );*/
+                Double d5 = ( (d3)/(d3+d4)) * 100 ;  // vote 1 percentage
+                Double d6 = ( (d4)/(d3+d4)) * 100 ;  // vote 2 percentage
 
-                // if (ikisi de gelmiş)
-                    hesapla();
+                int int5 = d5.intValue(); // test & backup
+                int int6 = d6.intValue(); // test & backup
+
+
+                System.out.println(d3);
+                System.out.println(d4);
+                System.out.println(d5);
+                System.out.println(d6);
+
+                percent1Progress.setProgress(int5);
+                percent2Progress.setProgress(int6);
+
+                vote1bartext.setText("%" + Double.toString(d5));    // percentage display
+                vote2bartext.setText("%" + Double.toString(d6));    //     "         "
+
             }
 
             @Override
@@ -102,23 +106,38 @@ public class database_page extends AppCompatActivity {
 
             }
         });
-        /*
 
-        int i3 = vote1Int.intValue();
+        vote1button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        int i4 = vote2Int.intValue();
+                vote1Int= vote1Int+1;
+                increment.child("vote1").setValue(vote1Int);
+            }
+        });
 
-        int i5=  ( i3/(i3+i4) )  *100 ;
 
-        int i6 =   ( i4/(i3+i4) )  *100 ;
+        vote2button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
-        percent1Progress.setProgress(i5);
-
-        percent1Progress.setProgress(i6); */
+                vote2Int= vote2Int+1;
+                increment.child("vote2").setValue(vote2Int);
+            }
+        });
 
 
     }
+
+
+ /*   public void savenumber(Long long1,int i,Double doub){
+
+        i=long1.intValue();
+        doub=long1.doubleValue();
+
+
+    }*/
+
 
 
 }
